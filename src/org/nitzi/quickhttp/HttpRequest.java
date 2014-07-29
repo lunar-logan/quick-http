6c9a490dc7709121caa8b03566fab79947fb41ef
lunar-logan <mrprogrammar@gmail.com>
2014-07-29 14:39:43 +0530

    Added Request, Response class

--- src/org/nitzi/quickhttp/HttpRequest.java
+++ src/org/nitzi/quickhttp/HttpRequest.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 public class HttpRequest {
-	public Map<String, List<String>> headers;
-	public String data;
-	
-	public String toString() {
-		return headers.toString() + "\n\n" + data;
-	}
+    public Map<String, List<String>> headers;
+    public String data;
+
+    public String toString() {
+        return headers.toString() + "\n\n" + data;
+    }
 }
